1f691878769365333a0fecf515e9cf47696065b7
raghav <raghav.gupta.gpt@gmail.com>
2022-03-18 23:21:03 +0530

    feat(mainlayout): add main layout design
    1. Main layout design based on flex
    2. Toggle functionality for visualizer

---
+++ src/routes/App.tsx
@@ -0,0 +1,12 @@
+import '../App.css';
+import { MainLayout } from '../components/Layout/MainLayout';
+
+function App() {
+  return (
+    <>
+      <MainLayout visualizerMode={true}/>
+    </>
+  );
+}
+
+export default App;
