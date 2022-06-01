8b6c38573d97bc66dbde2b8d43b4c815ad9ea1bf
raghav <raghav.gupta.gpt@gmail.com>
2022-06-01 12:24:01 +0530

    feat(trackerState): add reducer

--- src/core-ui/Tracker/index.tsx
+++ src/core-ui/Tracker/index.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRotateRight } from '@fortawesome/free-solid-svg-icons'
+import { State } from '../../store'
+import { useSelector } from 'react-redux'
 
 export const Tracker = () => {
   const Container = styled.div`
@@ -10,6 +12,8 @@ export const Tracker = () => {
     font-family: Arial, Helvetica, sans-serif;
     top: 60%;
   `
+  const trackerState = useSelector((state: State) => state.tracker)
+
   return (
     <Container className="flex flex-col-reverse text-center border border-sky-400 px-2 py-4 absolute left-6">
       <div>
@@ -17,11 +21,11 @@ export const Tracker = () => {
       </div>
       <div className="my-4">
         <div>Hits</div>
-        <div>123</div>
+        <div>{trackerState.hits}</div>
       </div>
       <div className="my-4">
         <div>Misses</div>
-        <div>2</div>
+        <div>{trackerState.misses}</div>
       </div>
     </Container>
   )
